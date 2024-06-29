38423239e5d5476d20be49246de91cdea450dbb9
hp <madel5505@gmail.com>
2024-06-29 04:48:40 -0700

    Final project ✔ ✔

---
+++ Project/Debug/FreeRTOS Source/croutine.d
@@ -0,0 +1,22 @@
+FreeRTOS Source/croutine.o: ../FreeRTOS\ Source/croutine.c \
+ C:\Users\hp\Desktop\RTOS\New\ folder\Demo\FreeRTOS\ Source\include/FreeRTOS.h \
+ ../include/FreeRTOSConfig.h ../system/include/cmsis/system_stm32f4xx.h \
+ C:\Users\hp\Desktop\RTOS\New\ folder\Demo\FreeRTOS\ Source\include/projdefs.h \
+ C:\Users\hp\Desktop\RTOS\New\ folder\Demo\FreeRTOS\ Source\include/portable.h \
+ C:\Users\hp\Desktop\RTOS\New\ folder\Demo\FreeRTOS\ Source\include/deprecated_definitions.h \
+ C:\Users\hp\Desktop\RTOS\New\ folder\Demo\FreeRTOS\ Source\portable\GCC\ARM_CM4F/portmacro.h \
+ C:\Users\hp\Desktop\RTOS\New\ folder\Demo\FreeRTOS\ Source\include/mpu_wrappers.h \
+ C:\Users\hp\Desktop\RTOS\New\ folder\Demo\FreeRTOS\ Source\include/task.h \
+ C:\Users\hp\Desktop\RTOS\New\ folder\Demo\FreeRTOS\ Source\include/list.h \
+ C:\Users\hp\Desktop\RTOS\New\ folder\Demo\FreeRTOS\ Source\include/croutine.h
+C:\Users\hp\Desktop\RTOS\New\ folder\Demo\FreeRTOS\ Source\include/FreeRTOS.h:
+../include/FreeRTOSConfig.h:
+../system/include/cmsis/system_stm32f4xx.h:
+C:\Users\hp\Desktop\RTOS\New\ folder\Demo\FreeRTOS\ Source\include/projdefs.h:
+C:\Users\hp\Desktop\RTOS\New\ folder\Demo\FreeRTOS\ Source\include/portable.h:
+C:\Users\hp\Desktop\RTOS\New\ folder\Demo\FreeRTOS\ Source\include/deprecated_definitions.h:
+C:\Users\hp\Desktop\RTOS\New\ folder\Demo\FreeRTOS\ Source\portable\GCC\ARM_CM4F/portmacro.h:
+C:\Users\hp\Desktop\RTOS\New\ folder\Demo\FreeRTOS\ Source\include/mpu_wrappers.h:
+C:\Users\hp\Desktop\RTOS\New\ folder\Demo\FreeRTOS\ Source\include/task.h:
+C:\Users\hp\Desktop\RTOS\New\ folder\Demo\FreeRTOS\ Source\include/list.h:
+C:\Users\hp\Desktop\RTOS\New\ folder\Demo\FreeRTOS\ Source\include/croutine.h:
